fa58b68b07e6bc94b8f2a64108dc255f0a640f98
Yash Bhardwaj <imybto@gmail.com>
2022-07-29 14:35:26 -0700

    error fixed

--- portfolio-app/components/layout.tsx
+++ portfolio-app/components/layout.tsx
@@ -12,8 +12,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="flex flex-col h-screen justify-between font-content antialiased">
      
       <main className="mb-auto">{children}</main>
-      <Footer />
-
+      
+    </div>
   )
 }
 
